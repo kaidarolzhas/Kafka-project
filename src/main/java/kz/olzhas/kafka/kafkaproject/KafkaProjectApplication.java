89ad2cb96e651f974c25b444a261c3dbaa263f75
zein <y.zeinullayev@ecc.kz>
2024-03-08 18:39:20 +0500

    first

---
+++ src/main/java/kz/olzhas/kafka/kafkaproject/KafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.olzhas.kafka.kafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/olzhas/kafka/kafkaproject/KafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.olzhas.kafka.kafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
